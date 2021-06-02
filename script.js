71dcabdb36d47bfae684c5ffc10048d24d6b7358
Demontier <demontier@outlook.com>
2021-06-01 22:16:15 -0300

    Requisitos 1 e 2

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = (function() {
+  let colorPalette = document.querySelectorAll('.color');
+  colorPalette[0].style.backgroundColor = 'green';
+  colorPalette[1].style.backgroundColor = 'yellow';
+  colorPalette[2].style.backgroundColor = 'blue';
+  colorPalette[3].style.backgroundColor = 'red';
+})
